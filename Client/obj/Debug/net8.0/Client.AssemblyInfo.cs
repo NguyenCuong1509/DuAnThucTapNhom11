874e8012428e6f2ad822ae81f9ad9cdf384fa485
QuangMinh <minh2k55555@gmail.com>
2025-05-28 20:39:47 +0700

    Add table

--- Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1459b59cdd9adb35e1ba43178c5d00d738579e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+974ca65dbaf9c9052e18bfe2c4d32290a154a91d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
