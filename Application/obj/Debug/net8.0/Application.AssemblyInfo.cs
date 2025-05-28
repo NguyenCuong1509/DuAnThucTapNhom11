87f47ee6575d044b7713a30871820e96563a95b3
QuangMinh <minh2k55555@gmail.com>
2025-05-28 20:45:12 +0700

    Add table

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+974ca65dbaf9c9052e18bfe2c4d32290a154a91d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874e8012428e6f2ad822ae81f9ad9cdf384fa485")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
