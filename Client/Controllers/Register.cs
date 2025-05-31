b57c8135e352732791c1982a5b499f8952330e3d
OnlyK <khai5108@gmail.com>
2025-05-31 19:21:55 +0700

    Update_View_Dangky

---
+++ Client/Controllers/Register.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class Register : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
